ea8c273cb6ef65806e4b0ed9df617eb6760e299b
skuahlin <sebastian.adams.mail@web.de>
2024-03-18 12:19:02 +0100

    Add form validation to invoice update

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -68,13 +68,23 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Some fields do not contain correct values. Failed to update Invoice.',
+    };
+  }
  
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
  
   try {
